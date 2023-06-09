96be4899b4e4ab3a7f1c7ac2df761a50fb79337b
bobmgr <bobbymgr710@gmail.com>
2023-06-09 17:36:56 +0545

    initial commit

---
+++ admin/manage-admin.php
@@ -0,0 +1,41 @@
+
+<?php include('partials/menu.php'); ?>
+
+        <!--Main content section starts-->
+        <div class="main-content">
+             <div class="wrapper">
+               <h1>Manage Admin</h1>
+</br></br>
+                 <!--button to add Admin-->
+                
+                   <a href="#" class="btn-primary">Add Admin</a>
+               
+</br></br>
+
+             <table class="tbl-full">
+              <tr>
+                 <th>S.N.</th>
+                 <th>Full Name</th>
+                 <th>Username</th>
+                 <th>Actions</th>
+              </tr>
+              <tr>
+                <td>1.</td>
+                <td>Bobby Magar</td>
+                <td>BobMgr</td>
+                
+                <td>
+                <a href="#" class="btn-secondary">Update Admin</a>  
+                  Delete Admin
+                </td>
+              </tr>
+             </table>
+
+
+              
+            </div>
+        </div>
+        <!--Main content section ends-->
+        <?php include('partials/footer.php'); ?>
+        
+    
